5ced5c0cfe2654d4e58d3a17e12adbafeb488d62
Sub-mar1n <y.shintaro.h12@gmail.com>
2024-03-22 21:11:34 +0900

    add : builtまで

---
+++ build/temp.linux-x86_64-3.10/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/sub-mar1n/YDLidar-SDK
+# Build directory: /home/sub-mar1n/YDLidar-SDK/build/temp.linux-x86_64-3.10
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("core")
+subdirs("src")
+subdirs("examples")
